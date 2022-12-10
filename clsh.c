1dec791c29bfad33845fd87995d492c66942b73b
ChangHoon Sung <changhoon.dev@gmail.com>
2022-12-11 07:42:00 +0900

    Add connection error handling

--- clsh.c
+++ clsh.c
@@ -14,7 +14,7 @@
 #define DEBUG_PRINT(fmt, args...) fprintf(stderr, "<DEBUG>: %s:%d:%s(): " fmt, \
     __FILE__, __LINE__, __func__, ##args)
 #else
-#define DEBUG_PRINT(fmt, args...)  ((void)0)
+#define DEBUG_PRINT(fmt, args...)
 #endif
 
 #define RD 0
@@ -28,10 +28,11 @@ extern int optind;
 char host[MAX_HOST][FILENAME_MAX];
 struct pollfd hostpfds[3][MAX_HOST];
 pid_t hostpid[MAX_HOST];
+int host_count;
 
 int out_redirected = 0, err_redirected = 0;
 
-int host_count, alive;
+volatile sig_atomic_t alive;    // signal 내부에서 쓰기 수행
 
 pid_t ssh_proc_open(char *hostname, char *command, int *to, int *from, int *err) {
     int to_pipe[2], from_pipe[2], err_pipe[2];
@@ -138,17 +139,21 @@ void open_redirection(const int STD_FD, char redirection_path[][PATH_MAX], int r
 }
 
 void sa_sigchld_handler(int sig) {
+    DEBUG_PRINT("SIGCHILD HANDLER\n");
     int status;
     pid_t pid;
 
     // 중첩된 시그널 처리를 위해 loop 적용
     while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        DEBUG_PRINT("WAITPID SUCCESS\n");
         if (pid <= 0) break; // 0: 자식이 있지만 아직 종료되지 않음, -1: 종료시킬 자식이 없음
+        DEBUG_PRINT("PID TP REAP: %d\n", pid);
         for (int i = 0; i < host_count; i++) {
+            DEBUG_PRINT("SIGCHLD ALIVE LOOP i:%d, hostpid[i]: %d, pid: %d\n", i, hostpid[i], pid);
             if (hostpid[i] == pid) {
-                DEBUG_PRINT("[%s] pid %d exited with status %d (%s)\n", host[i], pid, status,
-                            WIFEXITED(status) ? "normal" : "abnormal");
-                alive--;
+                DEBUG_PRINT("[%s] pid %d exited with status %d\n", host[i], pid, status);
+                --alive;
+                DEBUG_PRINT("alive: %d\n", alive);
                 break;
             }
         }
@@ -156,13 +161,25 @@ void sa_sigchld_handler(int sig) {
 }
 
 void sa_sigquit_handler(int sig) {
+    DEBUG_PRINT("SIGQUIT HANDLER\n");
     char sq = 3;
     for (int i = 0; i < host_count; i++) {
-        write(hostpfds[STDIN_FILENO][i].fd, &sq, sizeof(char));
+        if (hostpfds[STDIN_FILENO][i].fd != -1) {
+            if (write(hostpfds[STDIN_FILENO][i].fd, &sq, sizeof(char)) < 0) {
+                DEBUG_PRINT("write : %s\n", strerror(errno));
+            }
+        }
+    }
+    for (int i = 0; i < host_count; i++) {
+        if (hostpid[i] != -1) {
+            if (kill(hostpid[i], SIGINT) == -1) {
+                DEBUG_PRINT("kill : %s\n", strerror(errno));
+            }
+        }
     }
 }
 
-ssize_t print_pipe(int fd, int redirection_fd, FILE *master_fp, int host_no, int redirected) {
+ssize_t consume_pipe(int fd, int redirection_fd, FILE *master_fp, int host_no, int redirected) {
     int flag = 0;
     char buf[PIPE_BUFSIZ];
     ssize_t n = 0, total = 0;
@@ -175,6 +192,18 @@ ssize_t print_pipe(int fd, int redirection_fd, FILE *master_fp, int host_no, int
             flag = 1;
         }
         buf[n] = '\0';
+
+        // naive connection error 처리
+        if (strstr(buf, "Timeout") != NULL ||
+            strstr(buf, "timed out") != NULL ||
+            strstr(buf, "closed by remote host") != NULL ||
+            strstr(buf, "refused") != NULL) {
+            fprintf(stderr, "[%s]: Connection Error Detected!\n", host[host_no]);
+            hostpfds[STDIN_FILENO][host_no].fd = -1;
+            sa_sigquit_handler(SIGQUIT);
+            return total;
+        }
+
         if (!redirected) {
             fprintf(master_fp, "%s", buf);
         } else {
@@ -287,14 +316,14 @@ int main(int argc, char *argv[]) {
     struct sigaction sa_sigchld, sa_sigterm, sa_sigquit;
 
     sa_sigchld.sa_handler = sa_sigchld_handler;
-    sa_sigchld.sa_flags = SA_RESTART;   // poll timeout이 끝나기 전에 SIGCHLD가 발생한 경우 poll을 다시 호출할 수 있도록 SA_RESTART 설정
-    sigemptyset(&sa_sigchld.sa_mask);
+    sa_sigchld.sa_flags = SA_NOCLDSTOP;     // 자식의 STOP/CONT가 아닌, 종료에만 관심이 있음
+    sigfillset(&sa_sigchld.sa_mask);
     if (sigaction(SIGCHLD, &sa_sigchld, NULL) == -1) {
         perror("sigaction(sigchld)");
         exit(EXIT_FAILURE);
     }
 
-    sa_sigterm.sa_handler = SIG_IGN;    // 자식이 하던 일을 마칠 수 있도록 전파 방지 및 무시
+    sa_sigterm.sa_handler = SIG_IGN;    // 자식이 하던 일을 마칠 수 있도록 시그널 무시
     sa_sigterm.sa_flags = 0;
     sigemptyset(&sa_sigterm.sa_mask);
     if (sigaction(SIGTERM, &sa_sigterm, NULL) == -1) {
@@ -304,7 +333,7 @@ int main(int argc, char *argv[]) {
 
     sa_sigquit.sa_handler = sa_sigquit_handler;
     sa_sigquit.sa_flags = 0;
-    sigemptyset(&sa_sigquit.sa_mask);
+    sigfillset(&sa_sigquit.sa_mask);    // 모든 시그널 블럭
     if (sigaction(SIGQUIT, &sa_sigquit, NULL) == -1) {
         perror("sigaction(sigquit)");
         exit(EXIT_FAILURE);
@@ -348,17 +377,16 @@ int main(int argc, char *argv[]) {
     FILE *master_fps[3] = {stdin, stdout, stderr};
     ssize_t n;
     while (alive > 0) {
+        DEBUG_PRINT("alive(%d) loop\n", alive);
         // poll stdout and stderr
         for (int fd_no = 1; fd_no < 3; fd_no++) {
-            if (poll(hostpfds[fd_no], (nfds_t) host_count, 1) < 0) {
+            if (poll(hostpfds[fd_no], (nfds_t) host_count, 100) < 0) {
                 // SIGCHLD handler의 SA_RESTART에도 불구하고 poll과 같이 timeout 기능이 있는 syscall은 EINTR을 반환하는 경우가 있음
                 if (errno == EINTR) {
                     // perror가 interrupted인 경우 SIGCHLD에 의해 poll이 중단됨
                     // perror가 no child process인 경우 sigchld_handler의 waitpid에서 errno가 넘어온 것.
                     // 두 경우 모두 무시 가능
-#ifdef DEBUG
-                    perror("<DEBUG>: poll or waitpid");
-#endif
+                    DEBUG_PRINT("poll or waitpid: %s\n", strerror(errno));
                     continue;
                 }
             }
@@ -367,7 +395,7 @@ int main(int argc, char *argv[]) {
                 if (hostpfds[fd_no][i].revents & POLLHUP) {
                     switch (fd_no) {
                         case STDERR_FILENO:
-                            print_pipe(
+                            consume_pipe(
                                     hostpfds[STDERR_FILENO][i].fd,
                                     redirection_fd[STDERR_FILENO][i],
                                     master_fps[STDERR_FILENO],
@@ -383,7 +411,7 @@ int main(int argc, char *argv[]) {
                             break;
                         case STDOUT_FILENO:
                             // print all stdout first
-                            print_pipe(
+                            consume_pipe(
                                     hostpfds[STDOUT_FILENO][i].fd,
                                     redirection_fd[STDOUT_FILENO][i],
                                     master_fps[STDOUT_FILENO],
@@ -402,7 +430,7 @@ int main(int argc, char *argv[]) {
                             break;
                     }
                 } else if (hostpfds[fd_no][i].revents & POLLIN) {
-                    n = print_pipe(
+                    n = consume_pipe(
                             hostpfds[fd_no][i].fd,
                             redirection_fd[fd_no][i],
                             master_fps[fd_no],
